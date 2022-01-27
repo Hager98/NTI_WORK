513d18570a5ca129dff4bf138c27421447757eb1
hager98 <hager.sami98.hs@gmail.com>
2022-01-27 10:58:10 +0200

    test

---
+++ LED_CFG.h
@@ -0,0 +1,20 @@
+/*
+ * LED_CFG.h
+ *
+ * Created: 1/4/2022 10:02:42 AM
+ *  Author: master
+ */ 
+
+
+#ifndef LED_CFG_H_
+#define LED_CFG_H_
+
+
+/******************************Macros*******************************************/
+#define  B_LED_PIN  PC2
+#define  G_LED_PIN  PC1
+#define  R_LED_PIN  PC0
+/*******************************************************************************/
+
+
+#endif /* LED_CFG_H_ */
